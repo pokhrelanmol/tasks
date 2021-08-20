eba34bd8a645e2317be26e027097ef070eb05210
Anmol <anmolpokhrel46@gmail.com.com>
2021-08-20 14:06:45 +0530

    progress circle completed

--- src/Components/ProgressCircle.jsx
+++ src/Components/ProgressCircle.jsx
@@ -2,24 +2,36 @@ import React, { useEffect, useState } from "react";
 import "../css/progressCircle.css";
 
 const ProgressCircle = () => {
+  const [value, setvalue] = useState('');
+  const [stroke, setStroke] = useState("440");
+  const handleChange = (e) => {
+    setvalue(e.target.value);
+    setStroke(value?440-value.length*2.44:440)
+  };
+
+  // useEffect(() => {
+  //   setStroke(`${stroke - 2.44}`);
+  // }, [value]);
+
   return (
     <>
-    <input type="text"  />
-      <div className="circular">
-        <div className="inner"></div>
-        <div className="circle">
-           <div className="bar left">
-              <div className="progress">
-
-              </div>
-           </div>
-<div className="bar right">
-              <div className="progress">
-
-              </div>
-           </div>
+      <input
+        type="text"
+        value={value}
+        onChange={handleChange}
+        maxLength="180"
+      />
+      <div className="box">
+          <svg>
+            <circle cy="70" cx="70" r="70"></circle>
+            <circle
+              cy="70"
+              cx="70"
+              r="70"
+              style={{ strokeDashoffset: `${stroke}`, stroke: "black" }}
+            ></circle>
+          </svg>
         </div>
-      </div>
     </>
   );
 };
