67721f242d1ab29652bee0f20f1d4503654f6545
Anmol <anmolpokhrel46@gmail.com.com>
2021-08-20 13:26:47 +0530

    bare bone of progress circle using pure css

--- src/Components/ProgressCircle.jsx
+++ src/Components/ProgressCircle.jsx
@@ -1,39 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import '../css/progressCircle.css'
- 
+import React, { useEffect, useState } from "react";
+import "../css/progressCircle.css";
 
 const ProgressCircle = () => {
-   const [value,setvalue] = useState("")
-   const[stroke,setStroke] = useState("442.444")
-const handleChange =(e)=>{
-setvalue(e.target.value)
-}
+  return (
+    <>
+    <input type="text"  />
+      <div className="circular">
+        <div className="inner"></div>
+        <div className="circle">
+           <div className="bar left">
+              <div className="progress">
 
-useEffect(()=>{
-   
-
-setStroke(`${stroke-2.44}`)
-
-},[value])
-
-
-
-
-     return (
-           <>
-           <input type="text" value={value} onChange={handleChange} maxLength="180"/>
-           <div className="box">
-           <div className="percent">
-
-           <svg>
-              <circle cy="70" cx="70" r="70"></circle>
-              <circle cy="70" cx="70" r="70" style={{strokeDashoffset:`${stroke}`,stroke:"violet"}}></circle>
-           </svg>
-
+              </div>
            </div>
-           </div>
-</>
-     )
-}
+<div className="bar right">
+              <div className="progress">
 
-export default ProgressCircle
+              </div>
+           </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProgressCircle;
